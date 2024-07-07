7aa90c3b955141b0de7f8d428aaae4ae84c1c0d4
s-k3nyyy <collo.c.heg@gmail.com>
2024-07-07 20:52:25 +0300

    update

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,40 +1,42 @@
 import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import axios from 'axios'; // Import axios for making HTTP requests
-import './Login.css'; // Optional: Import CSS for styling
+import './Login.css';
 
 const Login = ({ onLogin }) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [message, setMessage] = useState(null); // State to hold success or error message
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
-  e.preventDefault();
+    e.preventDefault();
 
-  const userData = {
-    username,
-    password,
-  };
+    const userData = {
+      username,
+      password,
+    };
 
-  try {
-    const response = await axios.post('http://localhost:5000/login', userData);
-    console.log(response.data); // Log the response for debugging
-    onLogin(); // Update parent state to set isAuthenticated
-    navigate('/home', { replace: true }); // Redirect to home page after successful login
-  } catch (error) {
-    if (error.response && error.response.status === 401) {
-      alert('Login failed. Incorrect username or password.'); // Show specific error to the user
-    } else {
-      alert('Login failed. Please try again later.'); // Generic error message
+    try {
+      const response = await axios.post('http://localhost:5000/login', userData);
+      console.log(response.data); // Log the response for debugging
+      setMessage('Login successful');
+      onLogin(); // Update parent state to set isAuthenticated
+      navigate('/home', { replace: true }); // Redirect to home page after successful login
+    } catch (error) {
+      if (error.response && error.response.status === 401) {
+        setMessage('Invalid username or password'); // Set error message
+      } else {
+        setMessage('Login failed. Please try again later.'); // Generic error message
+      }
+      console.error('Login Error:', error); // Log the error for debugging
     }
-    console.error('Login Error:', error); // Log the error for debugging
-  }
-};
-
+  };
 
   return (
     <div className="Login">
       <h2>Login</h2>
+      {message && <p style={{ color: message.includes('successful') ? 'blue' : 'red' }}>{message}</p>}
       <form onSubmit={handleSubmit}>
         <label>
           Username:
