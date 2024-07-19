5d34e9679d51eb9b2242693b65ae14543686df05
s-k3nyyy <collo.c.heg@gmail.com>
2024-07-19 14:02:06 +0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: 'index.html',
+    },
+  },
+  base: '/<https://github.com/s-k3nyyy/s-k3nyyy-phase4-project-frontend-client.github.io.git>/', 
+});
